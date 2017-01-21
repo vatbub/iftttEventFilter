ca82c00b0fe294afda9cc9964361a4ce4389ef6b
vatbub <vatbub123+github@googlemail.com>
2017-01-21 01:03:34 +0100

    Continued that quick and dirty implementation

--- src/main/java/com/github/vatbub/web/ifttteventfilter/API.java
+++ src/main/java/com/github/vatbub/web/ifttteventfilter/API.java
@@ -21,29 +21,100 @@ package com.github.vatbub.web.ifttteventfilter;
  */
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import common.Internet;
+import org.apache.commons.lang.exception.ExceptionUtils;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * A quick and dirty draft of the api
  */
 public class API extends HttpServlet {
     private static final String passRegexp = "[CD]+";
-    private static final String jsonSkeleton = "{\n" +
-            "  \"passed\":\"/val/\"\n" +
-            "}";
+    private static final String makerEventName = "pushalotevent";
+    private static final String makerAPiKey = "dbjf67CBpZit4QOBthB0xW";
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/json");
-
-        boolean res = true;
+        PrintWriter writer;
 
         try {
-            response.getWriter().write(jsonSkeleton.replace("/val/", Boolean.toString(res)));
+            writer = response.getWriter();
         } catch (IOException e) {
             e.printStackTrace();
+            return;
+        }
+
+        Passed res = new Passed();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        // parse the request
+        if (!request.getContentType().equals("application/json")) {
+            // bad content type
+            Error error = new Error("content type must be application/json");
+            writer.write(gson.toJson(error));
+        }
+
+        StringBuilder requestBody = new StringBuilder();
+        String line;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
+        } catch (IOException e) {
+            Error error = new Error(e.getClass().getName() + " occurred while parsing the request", ExceptionUtils.getFullStackTrace(e));
+            writer.write(gson.toJson(error));
+            return;
+        }
+
+        PushalotJSONRequest pushalotJSONRequest = gson.fromJson(requestBody.toString(), PushalotJSONRequest.class);
+
+        res.passed = pushalotJSONRequest.message.matches(passRegexp);
+
+        // fire request to the maker channel
+        try {
+            Internet.sendEventToIFTTTMakerChannel(makerAPiKey, makerEventName, pushalotJSONRequest.message, pushalotJSONRequest.link, pushalotJSONRequest.imageURL);
+        } catch (IOException e) {
+            // Will only happen if I did a typo above
+            Error error = new Error(e.getClass().getName() + " occurred while parsing the request", ExceptionUtils.getFullStackTrace(e));
+            writer.write(gson.toJson(error));
+            return;
+        }
+
+        // Tell IFTTT the result of the operation
+        writer.write(gson.toJson(res));
+    }
+
+    class Passed {
+        boolean passed;
+    }
+
+    class PushalotJSONRequest {
+        String message;
+        String link;
+        String imageURL;
+    }
+
+    class Error {
+        String error;
+        String stacktrace;
+
+        Error(String error) {
+            this(error, "");
+        }
+
+        Error(String error, String stacktrace) {
+            this.error = error;
+            this.stacktrace = stacktrace;
         }
     }
 }
